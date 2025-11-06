273ae93ce0011901692c04287de80d5bf19a3a88
NikhileshKS <nickstack09@gmail.com>
2025-11-06 17:39:12 +0530

    backend added

---
+++ cars.js
@@ -0,0 +1,10 @@
+// Temporary cars data - this should be removed once proper API is implemented
+const cars = [
+    { id: 1, name: "Toyota Camry", country: "Japan" },
+    { id: 2, name: "BMW X5", country: "Germany" },
+    { id: 3, name: "Ford Mustang", country: "USA" },
+    { id: 4, name: "Honda Civic", country: "Japan" },
+    { id: 5, name: "Mercedes-Benz", country: "Germany" }
+];
+
+export default cars;
---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express' 
+import cars from './cars.js'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import cors from 'cors'
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// api routes
+app.get('/', (req, res) => {
+    res.send('API is Working')
+})
+
+app.get('/cars', (req, res) => {
+    res.json(cars) 
+})
+
+connectDB().then(() => {
+    app.listen(port,() => console.log("Server started at port " , port))
+})
