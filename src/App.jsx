a2c4115d9956fd71bcbab9b7ae99030bbc4338b9
Nikhilesh K S <nickstack09@gmail.com>
2026-02-27 19:13:22 +0530

    sidebar naviagate

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,42 @@
 import React from "react";
 import Login from "./pages/Login";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { AdminContext } from "./context/AdminContext";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Admin/Dashboard";
+import AllAppointment from "./pages/Admin/AllAppointment";
+import AddDoctor from "./pages/Admin/AddDoctor";
+import DoctorList from "./pages/Admin/DoctorList";
 
 const App = () => {
-
   const { aToken } = React.useContext(AdminContext);
 
   return aToken ? (
-    <div>
+    <div className="bg-[#F5F5FD]">
       <ToastContainer />
+      <Navbar />
+      <div className="flex items-start">
+        <Sidebar />
+        <div className="flex-1 p-5">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/admin-dashboard" element={<Dashboard />} />
+            <Route path="/all-appointments" element={<AllAppointment />} />
+            <Route path="/add-doctors" element={<AddDoctor />} />
+            <Route path="/doctor-list" element={<DoctorList />} />
+          </Routes>
+        </div>
+      </div>
     </div>
   ) : (
     <>
       <Login />
       <ToastContainer />
     </>
-  )
+  );
 };
 
 export default App;
