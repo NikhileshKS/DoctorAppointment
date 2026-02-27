42b446278c85224d9f97f003495c524f7e0d98d2
Nikhilesh K S <nickstack09@gmail.com>
2026-02-27 18:19:48 +0530

    fix cloudinary config

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,11 +1,10 @@
-import {v2 as cloudinary} from 'cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
 
 const connectCloudinary = () => {
-
     cloudinary.config({
-        cloud_name: process.env.ClOUNDARY_NAME,
-        api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret: process.env.CLOUNDIARY_SECRET_KEY,
+        cloud_name: process.env.CLOUDINARY_NAME,       
+        api_key: process.env.CLOUDINARY_API_KEY,      
+        api_secret: process.env.CLOUDINARY_SECRET_KEY,
     });
 }
 
