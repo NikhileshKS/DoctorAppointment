237ebf95f7edcfb10aca96ddf7272bdf8c90b34b
Nikhilesh K S <nickstack09@gmail.com>
2026-03-05 21:48:05 +0530

    add change-availablitiy

--- Routes/adminRoute.js
+++ Routes/adminRoute.js
@@ -2,6 +2,7 @@ import express from "express";
 import { addDoctor, adminLogin, allDoctors } from "../controllers/adminController.js";
 import upload from "../middlewares/multer.js";
 import authAdmin from "../middlewares/authAdmin.js";
+import { changeAvaliabity } from "../controllers/doctorControllers.js"; 
 
 const adminRouter = express.Router();
 
@@ -10,6 +11,9 @@ adminRouter.post("/login", adminLogin);
 
 // get all doctors from admin panel
 adminRouter.post("/all-doctors",authAdmin, allDoctors);
+
+// change doctor availability
+adminRouter.post("/change-availability", authAdmin,changeAvaliabity);
 
 // protected route
 adminRouter.post(
