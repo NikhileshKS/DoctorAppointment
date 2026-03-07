2ce843e1a142cf2b8d291385c037790dac12bd10
Nikhilesh K S <nickstack09@gmail.com>
2026-03-07 22:25:30 +0530

    Wrong key prop

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -15,16 +15,18 @@ const TopDoctors = () => {
             <div
             onClick={() => navigate(`/appointment/${item._id}`)}
             className="border border-blue-300 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
-            key={item.id || index}
+            key={item._id || index}
             >
             <img className='bg-blue-50' src={item.image} alt={`Doctor ${item.name}`} />
             <div className='p-4'>
                 <div className='flex items-center'>
-                    <span className='w-2 h-2 bg-green-600 rounded-full mr-2'></span>
-                    <p className='text-green-600 text-sm'>Available</p>
+                    <span className={`w-2 h-2 rounded-full mr-2 ${item.available ? 'bg-green-600' : 'bg-red-400'}`}></span>
+                    <p className={`text-sm ${item.available ? 'text-green-600' : 'text-red-400'}`}>
+                        {item.available ? 'Available' : 'Not Available'}
+                    </p>
                 </div>
                 <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
-                <p className='text-gray-800 text-sm'>{item.speciality}</p>
+                <p className='text-gray-800 text-sm'>{item.specialization}</p>
             </div>
         </div>
         ))}
