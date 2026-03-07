02ffe8606126ae7ed29173174ba8f928f63d99a0
Nikhilesh K S <nickstack09@gmail.com>
2026-03-07 12:56:12 +0530

    import tastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import { Route,Routes } from 'react-router-dom'
 import Home from './pages/Home'
@@ -10,9 +11,13 @@ import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10]'>
+    <ToastContainer />
     <Navbar/>
     <Routes>
       <Route path='/' element={<Home/>}/>
