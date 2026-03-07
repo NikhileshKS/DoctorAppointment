7813bb140eb51877bcc94a2bc996d740172afa8c
Nikhilesh K S <nickstack09@gmail.com>
2026-03-07 12:59:34 +0530

    add api of the list

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,13 +1,35 @@
 /* eslint-disable react/prop-types */
-import { createContext } from "react";
-import { doctors } from "../assets/assets";
+import { createContext, useState, useEffect } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-// Create the AppContext using createContext
 export const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
+    const backendURL = import.meta.env.VITE_BACKEND_URL;
+    const [doctors, setDoctors] = useState([]);
+
+    const getDoctorsData = async () => {
+        try {
+            const { data } = await axios.get(`${backendURL}/api/doctor/list`); 
+            if (data.success) {
+                setDoctors(data.data);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
+
+    useEffect(() => {
+        getDoctorsData();
+    }, []);
+
     const value = {
         doctors,
+        backendURL,      
+        getDoctorsData,
     };
 
     return (
