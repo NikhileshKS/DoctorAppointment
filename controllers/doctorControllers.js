98d9995eadacaad06ad60fd569590ac131631f3d
Nikhilesh K S <nickstack09@gmail.com>
2026-03-07 13:30:05 +0530

    add DoctorList

--- controllers/doctorControllers.js
+++ controllers/doctorControllers.js
@@ -1,21 +1,25 @@
 import doctorModel from "../models/doctorModel.js";
 
 const changeAvaliabity = async (req, res) => {
-
     try {
-        
         const { docId } = req.body;
-
         const docData = await doctorModel.findById(docId);
-
         await doctorModel.findByIdAndUpdate(docId, { available: !docData.available });
-
         return res.status(200).json({ success: true, message: "Doctor availability changed successfully" });
-
     } catch (error) {
         console.error(error);
         return res.status(500).json({ success: false, message: "changeAvaliabity Error" });
     }
 }
 
-export { changeAvaliabity };
+const doctorList = async (req, res) => {
+    try {
+        const doctors = await doctorModel.find({}).select(["-password", "-email"]);
+        return res.status(200).json({ success: true, data: doctors });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ success: false, message: "doctorList Error" });
+    }
+}
+
+export { changeAvaliabity, doctorList };
