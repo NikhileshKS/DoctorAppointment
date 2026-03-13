fc2dbd801cf8426f909e24110aaf5a2cdf281f6f
Nikhilesh K S <nickstack09@gmail.com>
2026-03-13 13:48:38 +0530

    add the footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
@@ -12,6 +13,7 @@ return (
         <SpecialityMenu/>
         <TopDoctors/>
         <Banner/>
+        <Footer/>
     </div>
 )
 }
