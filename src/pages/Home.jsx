aba5860ddfa8bbfbb4519936856a7f032027f1ec
Nikhilesh K S <nickstack09@gmail.com>
2026-03-14 11:13:55 +0530

    remove footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import TopDoctors from '../components/TopDoctors'
 import Banner from '../components/Banner'
-import Footer from '../components/Footer'
 
 const Home = () => {
 return (
@@ -13,7 +12,6 @@ return (
         <SpecialityMenu/>
         <TopDoctors/>
         <Banner/>
-        <Footer/>
     </div>
 )
 }
