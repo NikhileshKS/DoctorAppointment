3869dffe4809ae8e7ea607313be3a51593599de2
Nikhilesh K S <nickstack09@gmail.com>
2026-03-19 12:30:36 +0530

    add cancel appointment context

--- src/context/AdminContext.jsx
+++ src/context/AdminContext.jsx
@@ -10,6 +10,7 @@ const AdminContextProvider = ({ children }) => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const [doctors, setDoctors] = useState([]);
     const [aToken, setAToken] = useState(localStorage.getItem("aToken") || '');
+    const [appointments, setAppointments] = useState([]);
 
     const getAllDoctors = async () => {
         try {
@@ -47,13 +48,55 @@ const AdminContextProvider = ({ children }) => {
         }
     };
 
+    const getAllAppointments = async () => {
+        try {
+            
+            const { data } = await axios.post(
+                backendUrl + "/api/admin/appointments",
+                {},
+                { headers: { atoken: aToken } }
+            );
+            if (data.success) {
+                setAppointments(data.appointments);
+                console.log(data.appointments);
+            } else {
+                toast.error(data.message);
+            }
+
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
+
+    const cancelAppointment = async (appointmentId) => {
+    try {
+        const { data } = await axios.post(
+            backendUrl + "/api/admin/cancel-appointment",
+            { appointmentId },
+            { headers: { atoken: aToken } }
+        );
+        if (data.success) {
+            toast.success(data.message);
+            getAllAppointments(); 
+        } else {
+            toast.error(data.message);
+        }
+    } catch (error) {
+        toast.error(error.message);
+    }
+};
+
     const value = {
         aToken,
         setAToken,
         backendUrl,
         doctors,       
         getAllDoctors,  
-        changeAvailability
+        changeAvailability,
+        appointments,
+        getAllAppointments,
+        setAppointments,
+        cancelAppointment
     };
 
     return (
