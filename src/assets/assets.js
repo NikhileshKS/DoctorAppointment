5125226f29124d065ac37200bd5a0d70775eac3e
Nikhilesh K S <nickstack09@gmail.com>
2026-03-20 23:24:04 +0530

    reset about doctors

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -93,7 +93,7 @@ export const doctors = [
         speciality: 'General Physician',
         degree: 'MBBS',
         experience: '4 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: 'Dr. Chirag Deshpande is a dedicated General Physician with 4 years of experience in providing comprehensive medical care. He focuses on preventive healthcare, accurate diagnosis, and effective treatment plans to ensure the overall well-being of his patients. He is known for his patient-friendly approach and commitment to quality healthcare.',
         fees: '150 ₹',
         address: {
             line1: '17th Cross, Richmond',
@@ -107,7 +107,7 @@ export const doctors = [
         speciality: 'Gynecologist',
         degree: 'MBBS',
         experience: '3 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: 'Dr. Natasha Larson is a skilled Gynecologist with 3 years of experience in women’s healthcare. She specializes in reproductive health, prenatal care, and managing common gynecological conditions. She is dedicated to providing compassionate care, ensuring patient comfort, and promoting overall well-being through personalized treatment plans.',
         fees: '180 ₹',
         address: {
             line1: '27th Cross, Richmond',
@@ -121,7 +121,7 @@ export const doctors = [
         speciality: 'Dermatologist',
         degree: 'MBBS',
         experience: '1 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: 'Dr. Siddharth Patel is a dedicated Dermatologist with 1 year of experience in treating skin, hair, and nail conditions. He focuses on accurate diagnosis and effective treatment for issues such as acne, allergies, and infections. He aims to provide personalized care and help patients maintain healthy skin through proper guidance and treatment.',
         fees: '300 ₹',
         address: {
             line1: '37th Cross, Richmond',
@@ -135,7 +135,7 @@ export const doctors = [
         speciality: 'Pediatricians',
         degree: 'MBBS',
         experience: '2 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: 'Dr. Jameson is a caring Pediatrician with 2 years of experience in child healthcare. He focuses on monitoring growth and development, treating common childhood illnesses, and providing vaccinations. He is committed to creating a friendly and comfortable environment for children while guiding parents on maintaining their child’s health and well-being.',
         fees: '400 ₹',
         address: {
             line1: '47th Cross, Richmond',
@@ -149,7 +149,7 @@ export const doctors = [
         speciality: 'Neurologist',
         degree: 'MBBS',
         experience: '4 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: 'Dr. Jennifer Garcia is an experienced Neurologist with 4 years of expertise in diagnosing and treating disorders of the brain and nervous system. She specializes in managing conditions such as headaches, migraines, epilepsy, and nerve-related issues. She is dedicated to providing patient-focused care with accurate diagnosis and effective treatment plans.',
         fees: '550 ₹',
         address: {
             line1: '57th Cross, Richmond',
@@ -163,7 +163,7 @@ export const doctors = [
         speciality: 'Neurologist',
         degree: 'MBBS',
         experience: '4 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: 'Dr. Andrew Williams is a skilled Neurologist with 4 years of experience in diagnosing and treating neurological disorders. He focuses on conditions such as migraines, epilepsy, stroke management, and nerve-related disorders. He is committed to delivering precise diagnosis and personalized treatment to improve patients’ neurological health and quality of life.',
         fees: '500 ₹',
         address: {
             line1: '57th Cross, Richmond',
@@ -177,7 +177,7 @@ export const doctors = [
         speciality: 'General Physician',
         degree: 'MBBS',
         experience: '4 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: 'Dr. Christopher Davis is a dedicated General Physician with 4 years of experience in providing comprehensive healthcare. He focuses on preventive medicine, early diagnosis, and effective treatment of various medical conditions. He is known for his patient-centered approach and commitment to ensuring long-term health and well-being.',
         fees: '530 ₹',
         address: {
             line1: '17th Cross, Richmond',
@@ -191,7 +191,7 @@ export const doctors = [
         speciality: 'Gynecologist',
         degree: 'MBBS',
         experience: '3 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: 'Dr. Sharan is a dedicated Gynecologist with 3 years of experience in women’s health and reproductive care. She specializes in prenatal care, menstrual health, and managing common gynecological conditions. She is committed to providing compassionate and personalized care, ensuring patient comfort and well-being.',
         fees: '600 ₹',
         address: {
             line1: '27th Cross, Richmond',
@@ -205,7 +205,7 @@ export const doctors = [
         speciality: 'Dermatologist',
         degree: 'MBBS',
         experience: '1 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: 'Dr. Aira Sharma is a passionate Dermatologist with 1 year of experience in treating skin, hair, and nail conditions. She focuses on diagnosing issues such as acne, pigmentation, and allergies, and provides effective treatment plans. She is dedicated to helping patients achieve healthy skin through proper care and guidance.',
         fees: '370 ₹',
         address: {
             line1: '37th Cross, Richmond',
@@ -219,7 +219,7 @@ export const doctors = [
         speciality: 'Pediatricians',
         degree: 'MBBS',
         experience: '2 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: 'Dr. Jeffrey King is a dedicated Pediatrician with 2 years of experience in child healthcare. He focuses on treating common childhood illnesses, monitoring growth and development, and providing vaccinations. He ensures a friendly and supportive environment for children while guiding parents on maintaining their child’s health and well-being.',
         fees: '480 ₹',
         address: {
             line1: '47th Cross, Richmond',
@@ -233,7 +233,7 @@ export const doctors = [
         speciality: 'Gastroenterologist',
         degree: 'MBBS',
         experience: '4 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: 'Dr. Vasundhara Malhotra is an experienced Gastroenterologist with 4 years of expertise in diagnosing and treating digestive system disorders. She specializes in conditions such as acidity, ulcers, liver issues, and gastrointestinal infections. She is committed to providing accurate diagnosis and effective treatment while promoting long-term digestive health.',
         fees: '560 ₹',
         address: {
             line1: '57th Cross, Richmond',
@@ -247,7 +247,7 @@ export const doctors = [
         speciality: 'Neurologist',
         degree: 'MBBS',
         experience: '4 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: 'Dr. Amogh is an experienced Neurologist with 4 years of expertise in diagnosing and treating disorders of the brain and nervous system. He specializes in conditions such as migraines, epilepsy, and nerve-related issues. He is dedicated to providing precise diagnosis, effective treatment, and continuous care to improve patients’ quality of life.',
         fees: '367 ₹',
         address: {
             line1: '57th Cross, Richmond',
@@ -261,7 +261,7 @@ export const doctors = [
         speciality: 'General Physician',
         degree: 'MBBS',
         experience: '4 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: 'Dr. Chloe Evans is an experienced General Physician with 4 years of practice in diagnosing and treating a wide range of medical conditions. She focuses on preventive healthcare, early diagnosis, and personalized treatment plans. She is known for her attentive approach and commitment to ensuring the overall well-being of her patients.',
         fees: '850 ₹',
         address: {
             line1: '17th Cross, Richmond',
@@ -275,7 +275,7 @@ export const doctors = [
         speciality: 'Gynecologist',
         degree: 'MBBS',
         experience: '3 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: 'Dr. Ajith is a dedicated Gynecologist with 3 years of experience in women’s healthcare. He specializes in reproductive health, prenatal care, and the treatment of common gynecological conditions. He focuses on providing compassionate care and ensuring patient comfort through personalized treatment plans.',
         fees: '750 ₹',
         address: {
             line1: '27th Cross, Richmond',
@@ -289,7 +289,7 @@ export const doctors = [
         speciality: 'Dermatologist',
         degree: 'MBBS',
         experience: '1 Years',
-        about: 'Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies. Dr. Davis has a strong commitment to delivering comprehensive medical care, focusing on preventive medicine, early diagnosis, and effective treatment strategies.',
+        about: 'Dr. Amelia Hill is a passionate Dermatologist with 1 year of experience in treating skin, hair, and nail conditions. She focuses on issues such as acne, pigmentation, and skin allergies, providing effective and personalized treatment plans. She is dedicated to helping patients maintain healthy and confident skin.',
         fees: '650 ₹',
         address: {
             line1: '37th Cross, Richmond',
