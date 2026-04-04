15a578c5bdd4cd65537fee94dfee412a5961ef43
Nikhilesh K S <nickstack09@gmail.com>
2026-04-04 20:02:03 +0530

    small fix

--- src/pages/Doctor/DoctorAppointment.jsx
+++ src/pages/Doctor/DoctorAppointment.jsx
@@ -62,7 +62,7 @@ const DoctorAppointment = () => {
 
     useEffect(() => {
         if (dToken) getAppointments();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps 
     }, [dToken]);
 
     return (
